2d261565c3aa494877b1092c25a183ab23ff6186
root <www.long126@qq.com>
2019-10-08 15:02:51 +0800

    eslint 代码校验

---
+++ 前端框架/vue/代码校验/index.js
@@ -0,0 +1,9 @@
+//本页面阐述 vue 中用 eslint 做代码校验
+
+/** 
+ * 
+    1.在 vue 根目录下创建 .eslintrc.js 文件（规则可以去 eslint 官网查看）
+        官网地址：https://eslint.bootcss.com/docs/rules/
+    2.在 vscode 里面找到 eslint 插件的配置文件，把内容复制进去
+
+*/
